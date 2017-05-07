bdf83bf4683352af2971dd435eaeb7d76f7f45c6
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-05-06 21:54:18 -0500

    Finish draft brute

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -5,16 +5,29 @@ import java.util.TreeSet;
 public class BruteCollinearPoints {
     private int numSeg = 0;
     private LineSegment[] segment = null;
+    private Set<LineSegment> segmentSet = new TreeSet<LineSegment>();
     public BruteCollinearPoints(Point[] points) {
         checkDataSet(points);
-        for (int a = 0; a < points.length;a++) {
-            for (int b = a + 1; b < points.length; b++) {
-                for (int c = b + 1; c < points.length; c++) {
-                    for (int d = c + 1; c < points.length; d++) {
-
+        if (points.length >= 4) {
+            for (int a = 0; a < points.length;a++) {
+                for (int b = a + 1; b < points.length; b++) {
+                    for (int c = b + 1; c < points.length; c++) {
+                        for (int d = c + 1; c < points.length; d++) {
+                            Point[] points1 = new Point[4];
+                            points1[0] = points[a];
+                            points1[1] = points[b];
+                            points1[2] = points[c];
+                            points1[3] = points[d];
+                            if (checkCollinear(points1)) {
+                                LineSegment col = new LineSegment(points1[0], points1[3]);
+                                numSeg++;
+                                segmentSet.add(col);
+                            }
+                        }
                     }
                 }
             }
+            segment = segmentSet.toArray(new LineSegment[numSeg]);
         }
     }
 
@@ -32,6 +45,13 @@ public class BruteCollinearPoints {
         }
         return false;
     }
+
+    private boolean checkCollinear(Point[] points) {
+        Arrays.sort(points);
+        double slope = points[0].slopeTo(points[1]);
+        return slope == points[0].slopeTo(points[2]) && slope == points[0].slopeTo(points[3]);
+    }
+
     public int numberOfSegments() {
         return numSeg;
     }    // the number of line segments
