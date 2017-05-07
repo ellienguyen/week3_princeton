e31e6a1228584b5b99fa428d63883fea1ba9a73c
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-05-06 19:07:20 -0500

    Just try again

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -7,17 +7,18 @@ public class BruteCollinearPoints {
         if (points == null) {
             throw new java.lang.NullPointerException("Null input");
         }
-        for (int b = a + 1; b < points.length; b++) {
-            for (int c = b + 1; c < points.length; c++) {
-                for (int d = c + 1; c < points.length; d++) {
-
-                }
-            }
-        }
         for (int a = 0; a < points.length;a++) {
             if (points[a] == null) {
                 throw new java.lang.NullPointerException("Null input");
             } else {
+                for (int b = a + 1; b < points.length; b++) {
+                    for (int c = b + 1; c < points.length; c++) {
+                        for (int d = c + 1; c < points.length; d++) {
+
+                        }
+                    }
+                }
+            }
         }
 
     }
